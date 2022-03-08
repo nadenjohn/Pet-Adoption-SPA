b9132803cff2a88db223e657f2a09b81e6b0cedb
jwc20 <choij2015@gmail.com>
2022-03-08 14:54:36 -0800

    Added comment on what is searchable

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ const petFinderAPI = "http://localhost:3000/animals";
 
 fetch(petFinderAPI)
   .then((res) => res.json())
-  .then((json) => console.log(json))
+  .then(renderAnimals)
   .catch(console.err);
+
+function renderAnimals(animals) {
+  animals.forEach(renderAnimal);
+}
+
+function renderAnimal(animal) {
+  console.log(animal)
+  //console.log(animal.name);
+  //console.log(animal.colors.primary)
+}
+
+// Searchable 
+// {
+//   "species": "Cat",
+//   "type": "Cat",
+//   "age": "Baby",
+//   "gender": "Female",
+//   "size": "Large",
+// }
+
+
