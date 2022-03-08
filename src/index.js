7dabdf9e28f52efa10cfeca4fe139e96627b46dc
jwc20 <choij2015@gmail.com>
2022-03-08 12:52:10 -0800

    Check data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// add code here
+const petFinderAPI = "http://localhost:3000/animals";
+
+fetch(petFinderAPI)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch(console.err);
