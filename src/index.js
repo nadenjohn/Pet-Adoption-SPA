09d7904851a62fde20a3bd29b10e199cc9a1a0b7
jwc20 <choij2015@gmail.com>
2022-03-09 08:17:46 -0800

    Added image to result card

--- src/index.js
+++ src/index.js
@@ -10,38 +10,55 @@ function init() {
 function getAnimals() {
   fetch(petFinderAPI)
     .then((res) => res.json())
-    .then(renderAnimals)
+    .then(loadAnimals)
     .catch(console.err);
 }
 
-function renderAnimals(animals) {
-  animals.forEach(renderAnimal);
+function loadAnimals(animals) {
+  animals.forEach(updateAnimals);
 }
 
-function renderAnimal(animal) {
+function updateAnimals(animal) {
   // console.log(animal);
+  console.log(animal.photos.length);
+
   let ul = document.getElementById("animals-results");
   let animalItem = document.createElement("li");
   animalItem.className = "animal-item";
 
   let animalName = document.createElement("h3");
+
   let animalPhoto = document.createElement("img"); // list
+  if (animal.photos.length > 0) {
+    animalPhoto.src = animal.photos[0].small;
+    animalPhoto.alt = animal.name;
+  } else {
+    animalPhoto.src = "./assets/placeholder.gif";
+  }
+
   let animalColors = document.createElement("p"); // list
+
   let animalAge = document.createElement("p");
+
   let animalOrganization = document.createElement("p"); // organization_animal_id
+  
   let animalContact = document.createElement("p"); // list
+
   let animalGender = document.createElement('p');
+
   let animalSize = document.createElement('p');
+
   let animalCoat = document.createElement('p');
+
   let animalAttribute = document.createElement('p'); // might have to be a list
+
   let animalPrimaryBreed = document.createElement('p'); // animal.breeds.primary
   
   
   
-  console.log(animal.name);
 
   animalName.textContent = animal.name;
-  animalItem.append(animalName);
+  animalItem.append(animalName, animalPhoto);
   ul.append(animalItem);
 
 
